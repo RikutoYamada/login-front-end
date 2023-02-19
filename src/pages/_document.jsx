7a42ebce8cc197e2e749368b5a703724a9fef28a
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-02-19 22:47:05 +0900

    Edit _document.jsx for App background color

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className='bg-gray-200'>
+    <Html className='bg-gray-100'>
       <Head />
       <body>
         <Main />
