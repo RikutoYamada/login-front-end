ce5360190161f00fa0839cfb9c0c5614274d80e2
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-02-20 22:21:58 +0900

    Edit Button component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
+import Link from 'next/link'
+
 import Button from '@/components/elements/Button/Button'
 import Card from '@/components/elements/Card/Card'
 
 const Home = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Button>Button</Button>
-      <Card/>
-    </>
+    <div className='flex justify-center mt-36'>
+      <Card>
+        <div className='flex justify-center'>
+          <h1>ログインしていません</h1>
+        </div>
+        <div className='flex justify-end mt-10'>
+          <Link href='/signin' passHref>
+            <Button>ログイン</Button>
+          </Link>
+        </div>
+      </Card>
+    </div>
   )
 }
 
